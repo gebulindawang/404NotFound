a27c87995f2c6798cecfa9e093e5900f2aba805f
超级无敌哥布林大王 <15176388+super-invincible-goblin-king@user.noreply.gitee.com>
2025-10-18 10:32:46 +0800

    浩然提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,8 +8,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("晨阳宝宝我爱你");
-
-
     }
 
 }
---
+++ src/main/java/com/example/demo/haoran/haoran.java
@@ -0,0 +1,7 @@
+package com.example.demo.haoran;
+
+public class haoran {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
