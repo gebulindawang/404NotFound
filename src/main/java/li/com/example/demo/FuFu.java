a90501d60707918e0d7dac667d722c5885b141dc
root <1569827935@qq.com>
2025-10-18 19:28:40 +0800

    芙芙

--- src/main/java/li/com/example/demo/FuFu.java
+++ src/main/java/li/com/example/demo/FuFu.java
@@ -3,7 +3,7 @@ package li.com.example.demo;
 //~~~~~~~~~~~//
 
 public class FuFu {
-    public static  void li(){
+    public static  void main(String[] args) {
         System.out.println("芙芙");
     }
 }
