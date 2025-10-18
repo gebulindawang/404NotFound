e502d3e4600a56f78f97fb6150bd6a294cf517df
IFREELIFE <15824824734@139.com>
2025-10-18 15:35:41 +0800

    干员追加

---
+++ src/main/java/ran/com/example/demo/pojo/IFREE.java
@@ -0,0 +1,11 @@
+package ran.com.example.demo.pojo;
+
+import lombok.Data;
+
+@Data
+public class IFREE {
+    private String name;
+    private String url;
+    private String img;
+    private String type;
+}
