88e95731f4d16ffd559ab0943da2e608e6a0ca4f
超级无敌哥布林大王 <15176388+super-invincible-goblin-king@user.noreply.gitee.com>
2025-10-18 14:37:31 +0800

    我爱玩原神

---
+++ src/main/java/ran/com/example/demo/haoran/genshin.java
@@ -0,0 +1,14 @@
+package ran.com.example.demo.haoran;
+
+public class genshin {
+    public static void main(String[] args) {
+        sayGenshin();
+    }
+
+    public static void sayGenshin(){
+
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("我爱玩原神");
+        }
+    }
+}
